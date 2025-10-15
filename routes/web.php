683a7c0dde8caa2fb95562f01eb820ec20de67cc
maulifathiazahra <maulifatia@gmail.com>
2025-10-15 13:22:35 +0700

    Menggunakan Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/profile', function () {
-    return view('profile');
+Route::get('/', function ()  {
+    return view('welcome');
 });
 
 Route::get('/about', function () {
@@ -12,4 +12,14 @@ Route::get('/about', function () {
 
 Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('/profile', function () {
+    $nama = 'Mauli Fathia Zahra';
+    //return view('profile' , compact ('nama'));
+    return view('profile')->with('nama', $nama);
 });
