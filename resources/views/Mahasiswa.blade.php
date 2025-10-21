45842dbb9783433708839a5ab6806940a2b2a897
maulifathiazahra <maulifatia@gmail.com>
2025-10-21 16:35:38 +0700

    belajar mengenai tabel

--- resources/views/Mahasiswa.blade.php
+++ resources/views/Mahasiswa.blade.php
@@ -31,8 +31,41 @@
   </div>
 </nav>
 
-    <div class="container" style="margin-left:30px">
+    <div class="container">
     <h1>Ini adalah halaman Mahasiswa</h1>
+    
+  <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
+    <thead>
+      <tr>
+        <th>NPM</th>
+        <th>Nama Mahasiswa</th>
+        <th>Jenis Kelamin</th>
+        <th colspan="2">TTL</th>
+      </tr>
+    </thead>
+  </tbody>
+  <tr>
+        <td>123</td>
+        <td>Cantika</td>
+        <td>Perempuan</td>
+        <td>12-08-2007</td>
+        <td rowspan="3">Kota Medan</td>
+  </tr>
+   <tr>
+        <td>124</td>
+        <td>Zahra</td>
+        <td>Perempuan</td>
+        <td>21-06-2008</td>
+  </tr>  
+  <tr>
+        <td>125</td>
+        <td>Raziq</td>
+        <td>Laki-Laki</td>
+        <td>2-03-2009</td>
+  </tr>
+  </tbody>
+</table>
+
 </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
